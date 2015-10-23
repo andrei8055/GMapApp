0f4d1b1b62f5d8735ebfc4c8b044b8c83e45fe75
Andrei <agape.andrei93@yahoo.com>
2015-10-23 19:12:59 +0200

    new branch

--- index.php
+++ index.php
@@ -15,7 +15,6 @@
     <script src="http://code.highcharts.com/highcharts.js"></script>
     <script src="http://code.highcharts.com/modules/exporting.js"></script>
 
-
       <script>
 
     var GMapObj = new GMap();
